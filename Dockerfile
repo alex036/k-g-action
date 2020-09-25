a157a00907541a45531f8264ce6ddb9c6649b2c6
alex036 <asmith@waveapps.com>
2020-09-25 10:58:00 -0400

    added env vars

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM openjdk:8-jre-slim
 
-COPY bin/kafka-gitops /usr/src/kafka-gitops
+COPY bin/kafka-gitops /usr/local/bin/kafka-gitops
 COPY entrypoint.sh /entrypoint.sh
 
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,17 @@
 set -e
 
 export HOME="/github/workspace"
-chmod +x "/usr/src/kafka-gitops"
+chmod +x "/usr/local/bin/kafka-gitops"
+
+export KAFKA_SECURITY_PROTOCOL="SASL_SSL"
+export KAFKA_SASL_MECHANISM="PLAIN"
+export KAFKA_SSL_ENDPOINT_IDENTIFICATION_ALGORITHM="HTTPS"
+
+export KAFKA_BOOTSTRAP_SERVERS="$INPUT_KAFKA_BOOTSTRAP_SERVERS"
+export KAFKA_SASL_JAAS_USERNAME="$INPUT_KAFKA_SASL_JAAS_USERNAME"
+export KAFKA_SASL_JAAS_PASSWORD="$INPUT_KAFKA_SASL_JAAS_PASSWORD"
+export XX_CCLOUD_EMAIL="$INPUT_XX_CCLOUD_EMAIL"
+export XX_CCLOUD_PASSWORD="$INPUT_XX_CCLOUD_PASSWORD"
 
 # If a working directory is detected as input
 if [ -n "$INPUT_WORKINGDIRECTORY" ]
@@ -12,7 +22,8 @@ then
 fi
 
 # Do stuff
-message=$(/usr/src/kafka-gitops -f state.yaml --no-delete plan)
+#message=$(kafka-gitops -f state.yaml --no-delete plan)
+message=$(kafka-gitops -f state.yaml validate)
 echo "::set-output name=plan_output::$message"
 
 # If a working directory is detected as input, revert to the
